4385af9c6d2bef641f999da2ed067cec1c91de27
Dom Barter <dom.barter@3squared.com>
2021-08-26 09:54:41 +0100

    Add product reviews component

---
+++ src/components/ProductReviews.vue
@@ -0,0 +1,146 @@
+<template>
+  <span>
+    <h4 class="text-h6 mb-5 page-content">Reviews</h4>
+
+    <v-expansion-panels class="mb-7 page-content" v-model="reviewPanel">
+      <v-expansion-panel>
+        <v-expansion-panel-header color="secondary" class="rounded-b mb-n1">
+          Add a new review
+        </v-expansion-panel-header>
+        <v-expansion-panel-content color="secondary" class="rounded-b">
+          <v-form ref="form" @submit.prevent="submitReviewHandler">
+            <v-row>
+              <v-col class="ma-0 pb-0">
+                <v-text-field
+                  outlined
+                  label="Reviewer Name"
+                  v-model="review.reviewer"
+                  required
+                  :rules="rules.reviewerRules"
+                ></v-text-field>
+                <v-select
+                  :items="[1, 2, 3, 4, 5]"
+                  label="Star Rating"
+                  outlined
+                  v-model="review.starRating"
+                  required
+                  :rules="rules.starRatingRules"
+                ></v-select>
+              </v-col>
+              <v-col>
+                <v-textarea
+                  outlined
+                  label="Message"
+                  height="140"
+                  v-model="review.message"
+                  required
+                  :rules="rules.messageRules"
+                ></v-textarea>
+                <v-btn color="primary" class="float-end" type="submit">
+                  Submit Review
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-form>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
+
+    <p class="text-body1 page-content" v-if="reviews.length === 0">
+      There are no reviews for this product.
+    </p>
+
+    <v-card
+      elevation="2"
+      v-for="review in reviews"
+      :key="review.message"
+      class="mb-5 page-content"
+    >
+      <v-row>
+        <v-col class="col-auto mr-auto py-0">
+          <v-card-title class="pb-0">{{ review.reviewer }}</v-card-title>
+          <v-card-text>{{ review.message }}</v-card-text>
+        </v-col>
+        <v-col
+          class="col-auto py-0 justify-content-center align-center d-flex pe-10"
+        >
+          <div class="d-flex flex-row">
+            <v-icon color="warning" medium>mdi-star</v-icon>
+            <p class="text-h6 ma-0 warning--text">{{ review.starRating }}</p>
+          </div>
+        </v-col>
+      </v-row>
+    </v-card>
+  </span>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+import NProgress from "nprogress";
+
+export default {
+  data() {
+    return {
+      reviewPanel: null,
+      review: this.createFreshReview(),
+      rules: {
+        reviewerRules: [(v) => !!v || "Reviewer name is required"],
+        messageRules: [(v) => !!v || "Message is required"],
+        starRatingRules: [
+          (v) => !!v || "Star rating is required",
+          (v) => (v >= 1 && v <= 5) || "Star rating must be between 1 and 5",
+        ],
+      },
+    };
+  },
+  props: {
+    reviews: {
+      type: Array,
+      required: true,
+    },
+    productCode: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    ...mapActions("product", ["addReview"]),
+    createFreshReview() {
+      return {
+        reviewer: "",
+        message: "",
+        starRating: undefined,
+      };
+    },
+    submitReviewHandler() {
+      if (this.$refs.form.validate()) {
+        NProgress.start();
+        this.addReview({
+          code: this.productCode,
+          review: this.review,
+        })
+          .then(() => {})
+          .catch((error) => {
+            console.error(error);
+            if (error.response.status == 404) {
+              this.$router.push({
+                name: "404",
+                params: { resource: "product" },
+              });
+            } else {
+              this.$router.push({ name: "Network Issue" });
+            }
+          })
+          .finally(() => {
+            NProgress.done();
+            this.reviewPanel = null;
+            this.$refs.form.reset();
+            this.review = this.createFreshReview();
+          });
+      }
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/views/Product.vue
+++ src/views/Product.vue
@@ -14,101 +14,16 @@
 
     <v-divider class="mt-5 mb-5"></v-divider>
 
-    <h4 class="text-h6 mb-5 page-content">Reviews</h4>
-
-    <v-expansion-panels class="mb-7 page-content" v-model="reviewPanel">
-      <v-expansion-panel>
-        <v-expansion-panel-header color="secondary" class="rounded-b mb-n1">
-          Add a new review
-        </v-expansion-panel-header>
-        <v-expansion-panel-content color="secondary" class="rounded-b">
-          <v-form ref="form" @submit.prevent="submitReviewHandler">
-            <v-row>
-              <v-col class="ma-0 pb-0">
-                <v-text-field
-                  outlined
-                  label="Reviewer Name"
-                  v-model="review.reviewer"
-                  required
-                  :rules="rules.reviewerRules"
-                ></v-text-field>
-                <v-select
-                  :items="[1, 2, 3, 4, 5]"
-                  label="Star Rating"
-                  outlined
-                  v-model="review.starRating"
-                  required
-                  :rules="rules.starRatingRules"
-                ></v-select>
-              </v-col>
-              <v-col>
-                <v-textarea
-                  outlined
-                  label="Message"
-                  height="140"
-                  v-model="review.message"
-                  required
-                  :rules="rules.messageRules"
-                ></v-textarea>
-                <v-btn color="primary" class="float-end" type="submit">
-                  Submit Review
-                </v-btn>
-              </v-col>
-            </v-row>
-          </v-form>
-        </v-expansion-panel-content>
-      </v-expansion-panel>
-    </v-expansion-panels>
-
-    <p class="text-body1 page-content" v-if="reviews.length === 0">
-      There are no reviews for this product.
-    </p>
-
-    <v-card
-      elevation="2"
-      v-for="review in reviews"
-      :key="review.message"
-      class="mb-5 page-content"
-    >
-      <v-row>
-        <v-col class="col-auto mr-auto py-0">
-          <v-card-title class="pb-0">{{ review.reviewer }}</v-card-title>
-          <v-card-text>{{ review.message }}</v-card-text>
-        </v-col>
-        <v-col
-          class="col-auto py-0 justify-content-center align-center d-flex pe-10"
-        >
-          <div class="d-flex flex-row">
-            <v-icon color="warning" medium>mdi-star</v-icon>
-            <p class="text-h6 ma-0 warning--text">{{ review.starRating }}</p>
-          </div>
-        </v-col>
-      </v-row>
-    </v-card>
+    <ProductReviews :reviews="reviews" :productCode="product.productCode" />
   </div>
 </template>
 
 <script>
 import ProductActionButtons from "@/components/ProductActionButtons";
+import ProductReviews from "@/components/ProductReviews";
 import { animationsMixin } from "@/mixins/Animations";
-import { mapActions } from "vuex";
-import NProgress from "nprogress";
 
 export default {
-  data() {
-    return {
-      reviewPanel: null,
-      review: this.createFreshReview(),
-      rules: {
-        reviewerRules: [(v) => !!v || "Reviewer name is required"],
-        messageRules: [(v) => !!v || "Message is required"],
-        starRatingRules: [
-          (v) => !!v || "Star rating is required",
-          (v) => (v >= 1 && v <= 5) || "Star rating must be between 1 and 5",
-        ],
-      },
-    };
-  },
   props: {
     product: {
       type: Object,
@@ -119,44 +34,7 @@ export default {
       required: true,
     },
   },
-  methods: {
-    ...mapActions("product", ["addReview"]),
-    createFreshReview() {
-      return {
-        reviewer: "",
-        message: "",
-        starRating: undefined,
-      };
-    },
-    submitReviewHandler() {
-      if (this.$refs.form.validate()) {
-        NProgress.start();
-        this.addReview({
-          code: this.product.productCode,
-          review: this.review,
-        })
-          .then(() => {})
-          .catch((error) => {
-            console.error(error);
-            if (error.response.status == 404) {
-              this.$router.push({
-                name: "404",
-                params: { resource: "product" },
-              });
-            } else {
-              this.$router.push({ name: "Network Issue" });
-            }
-          })
-          .finally(() => {
-            NProgress.done();
-            this.reviewPanel = null;
-            this.$refs.form.reset();
-            this.review = this.createFreshReview();
-          });
-      }
-    },
-  },
   mixins: [animationsMixin],
-  components: { ProductActionButtons },
+  components: { ProductActionButtons, ProductReviews },
 };
 </script>
